509ac67d1e4046a05eabfef63cc46dab4389625b
somnoynadno <somnoynadno@yandex.ru>
2020-04-09 17:31:48 +0700

    Formulas added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import "./App.scss";
-import Header from "../Header/Header"
 import MainChart from "../MainChart/MainChart";
 
 export default class App extends React.Component{
     public render(){
         return(
             <div>
-                <Header name="header" count={0} />
                 <MainChart />
             </div>
         )
--- src/components/MainChart/MainChart.tsx
+++ src/components/MainChart/MainChart.tsx
@@ -10,112 +10,112 @@ interface IState{
 }
 
 var data = {
-    labels: ["0"],
+    labels: [],
     datasets: [
         {
             // 0
             label: "Заболеваемость (%)",
-            fillColor: "rgba(152,61,158,0.2)",
+            fillColor: "rgba(152,61,158,0.1)",
             strokeColor: "rgb(152,61,158,1)",
             pointColor: "rgba(152,61,158,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(152,61,158,1)",
-            data: [0]
+            data: []
         },
         {
             // 1
             label: "Финансирование (%)",
-            fillColor: "rgba(44,173,65,0.2)",
+            fillColor: "rgba(44,173,65,0.1)",
             strokeColor: "rgb(44,173,65,1)",
             pointColor: "rgba(44,173,65,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(44,173,65,1)",
-            data: [0]
+            data: []
         },
         {
             // 2
             label: "Медицинское оборудование",
-            fillColor: "rgba(100,187,205,0.2)",
+            fillColor: "rgba(100,187,205,0.1)",
             strokeColor: "rgba(100,187,205,1)",
             pointColor: "rgba(100,187,205,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(100,187,205,1)",
-            data: [0]
+            data: []
         },
         {
             // 3
             label: "Качество медицины (%)",
-            fillColor: "rgba(55,73,193,0.2)",
+            fillColor: "rgba(55,73,193,0.1)",
             strokeColor: "rgb(55,73,193,1)",
             pointColor: "rgba(55,73,193,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(55,73,193,1)",
-            data: [0]
+            data: []
         },
         {
             // 4
             label: "Смертность (%)",
-            fillColor: "rgba(198,33,33,0.2)",
+            fillColor: "rgba(198,33,33,0.1)",
             strokeColor: "rgba(198,33,33,1)",
             pointColor: "rgba(198,33,33,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(198,33,33,1)",
-            data: [0]
+            data: []
         },
         {
             // 5
             label: "Количество врачей",
-            fillColor: "rgba(13,177,106,0.2)",
+            fillColor: "rgba(13,177,106,0.1)",
             strokeColor: "rgb(13,177,106)",
             pointColor: "rgba(13,177,106,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(13,177,106,1)",
-            data: [0]
+            data: []
         },
         {
             // 6
             label: "Численность населения",
-            fillColor: "rgba(183,196,67,0.2)",
+            fillColor: "rgba(183,196,67,0.1)",
             strokeColor: "rgb(183,196,67, 1)",
             pointColor: "rgba(183,196,67,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(183,196,67,1)",
-            data: [0]
+            data: []
         },
         {
             // 7
             label: "Количество пациентов",
-            fillColor: "rgba(210,99,41,0.2)",
+            fillColor: "rgba(210,99,41,0.1)",
             strokeColor: "rgb(210,99,41,1)",
             pointColor: "rgba(210,99,41,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(210,99,41,1)",
-            data: [0]
+            data: []
         },
         {
             // 8
             label: "Рождаемость (%)",
-            fillColor: "rgba(131,246,123,0.2)",
+            fillColor: "rgba(131,246,123,0.1)",
             strokeColor: "rgb(131,246,123,1)",
             pointColor: "rgb(131,246,123,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(131,246,123,1)",
-            data: [0]
+            data: []
         }
     ]
 }
 
 export default class MainChart extends React.Component<IProps, IState> {
-    private time = 1;
+    private time = 0;
     private game: Game;
 
     constructor(props : IProps){
@@ -132,15 +132,28 @@ export default class MainChart extends React.Component<IProps, IState> {
         console.log(this.time);
         this.game.tick();
 
+        // fucking typescript
+        // I sure do care about your datatype
+        // pls fuck off
+        // @ts-ignore
         data.datasets[0].data.push(this.game.illness*100);
+        // @ts-ignore
         data.datasets[1].data.push(this.game.financing*100);
+        // @ts-ignore
         data.datasets[2].data.push(this.game.medicalEquipment);
+        // @ts-ignore
         data.datasets[3].data.push(this.game.medicineQuality*100);
+        // @ts-ignore
         data.datasets[4].data.push(this.game.mortality*100);
+        // @ts-ignore
         data.datasets[5].data.push(this.game.doctorsNum);
+        // @ts-ignore
         data.datasets[6].data.push(this.game.populationSize);
+        // @ts-ignore
         data.datasets[7].data.push(this.game.patientsNum);
+        // @ts-ignore
         data.datasets[8].data.push(this.game.fertility*100);
+        // @ts-ignore
         data.labels.push(this.time.toString());
 
         this.setState({
@@ -150,8 +163,24 @@ export default class MainChart extends React.Component<IProps, IState> {
         this.time++;
     }
     public render() {
+        // use full screen
         return (
-            <LineChart data={this.state.chartData} redraw width="600" height="250"/>
+            <div>
+                <LineChart data={this.state.chartData} redraw width="1000" height="700"/>
+                <div id={"panel"} style={{float: 'right', margin: '30px', textAlign: 'end'}}>
+                    <span>Death rate </span>
+                    <button>-</button>
+                    <input type="text" value="1" style={{width: '50px'}}/>
+                    <button>+</button>
+                    <br/>
+
+                    <span>Birth rate </span>
+                    <button>-</button>
+                    <input type="text" value="1" style={{width: '50px'}}/>
+                    <button>+</button>
+                    <br/>
+                </div>
+            </div>
         );
     }
 
